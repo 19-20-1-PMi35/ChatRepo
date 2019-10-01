8004317af9a7d3a85c8eb3e3c5afbaea87dfdf5a
ThreeEyedRaven3 <49842453+ThreeEyedRaven3@users.noreply.github.com>
2019-10-01 21:31:01 +0300

    Add files via upload

---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace InstantMessengerServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to close program.");
+            Console.ReadLine();
+        }
+
+        // IP of this computer. If you are running all clients at the same computer you can use 127.0.0.1 (localhost). 
+        public IPAddress ip = IPAddress.Parse("127.0.0.1");
+        public int port = 2000;
+        public TcpListener server;
+
+        public Dictionary<string, Client> users = new Dictionary<string, Client>();
+        public Program()
+        {
+            Console.Title = "InstantMessenger Server";
+            Console.WriteLine("----- InstantMessenger Server -----");
+            Console.WriteLine("[{0}] Starting server...", DateTime.Now);
+
+            server = new TcpListener(ip, port);
+            server.Start();
+            Console.WriteLine("[{0}] Server is running properly!", DateTime.Now);
+
+            Listen();
+        }
+
+        void Listen()  // Listen to incoming connections.
+        {
+
+            while (true)
+            {
+                TcpClient tcpClient = server.AcceptTcpClient();  // Accept incoming connection.
+                Client client = new Client(this, tcpClient);     // Handle in another thread.
+            }
+        }
+
+    }
+}
